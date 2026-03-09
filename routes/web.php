d2fab07e2845a1fa471f5d540432a7cd6424a7de
Igor Olejar <igor.olejar@gmail.com>
2026-03-09 14:12:16 +0000

    Making user verify email

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/ratings', [RatingController::class, 'store'])->name('ratings.store');
+});
