a0b68e8ed408327e30fe2a49ea4e69b5d1b4b3e4
Igor Olejar <igor.olejar@gmail.com>
2026-04-10 15:45:17 +0100

    Creating the Middleware that checks if the user is active

---
+++ app/Http/Middleware/CheckActiveStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\UserStatus;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckActiveStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->status !== UserStatus::ACTIVE) {
+            return redirect('/dashboard')->with('error', 'Your account must be approved by an admin before you can perform this action.');
+        }
+
+        return $next($request);
+    }
+}
