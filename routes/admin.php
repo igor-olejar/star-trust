c43fcc772bdd16f109329675413e0f3184c97b3b
Igor Olejar <igor.olejar@gmail.com>
2026-04-11 11:21:34 +0100

    Defining admin routes

---
+++ routes/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Auth\AdminAuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest:admin')->group(function () {
+    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('login.submit');
+});
+
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
+});
